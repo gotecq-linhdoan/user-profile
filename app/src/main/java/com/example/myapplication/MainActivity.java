bf189a8d2252361e763096053d7d70d06a464709
Tuan Linh <linh.doan@gotecq.com>
2023-10-13 15:40:13 +0700

    feat: user profile app

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button saveDetailsButton = findViewById(R.id.saveDetailsButton);
+
+        saveDetailsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveDetails();
+            }
+        });
+    }
+
+    private void saveDetails() {
+        // Creates an object of our helper class
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+
+        // Get references to the EditText views and read their content
+        EditText nameTxt = findViewById(R.id.nameText);
+        EditText dobTxt = findViewById(R.id.dobText);
+        EditText emailTxt = findViewById(R.id.emailText);
+
+        String name = nameTxt.getText().toString();
+        String dob = dobTxt.getText().toString();
+        String email = emailTxt.getText().toString();
+
+        // Calls the insertDetails method we created
+        long personId = dbHelper.insertDetails(name, dob, email);
+
+        // Shows a toast with the automatically generated id
+        Toast.makeText(this, "Person has been created with id: " + personId,
+                Toast.LENGTH_LONG
+        ).show();
+        Intent intent = new Intent(this, DetailsActivity.class);
+
+        startActivity(intent);
+
+    }
+}
